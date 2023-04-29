2d72363a2e8b49ff5dd78f2114596cd221c09d1a
yourMACARONi <sarpiuq@gmail.com>
2023-04-29 19:49:14 +0800

    main code udpate

--- userlog.js
+++ userlog.js
@@ -5,7 +5,7 @@ let password = document.getElementById("password")
 function Login() {
     if (username.value == localStorage.getItem("username1") && password.value == localStorage.getItem("password1")) {
         alert("Logged In Successfully")
-        location.href = '/index.html'
+        location.href = 'index.html'
     } else {
         alert("Username Or Password is Invalid")
     }
