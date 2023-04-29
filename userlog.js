414e1cda31a2920a58567815228c166b637b4b69
yourMACARONi <126305477+yourMACARONi@users.noreply.github.com>
2023-04-28 21:43:21 -0700

    Update userlog.js

--- userlog.js
+++ userlog.js
@@ -5,6 +5,6 @@ let password = document.getElementById("password")
 function Login() {
     if (username.value && password.value == 'admin') {
        alert("Logged In Successfully!")
-        window.location.assign("/index.html")
+        window.location.assign("index.html")
     }
 }
