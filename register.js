e8a74ce106256a2f07361d8a5a9117c1d067ae76
yourMACARONi <sarpiuq@gmail.com>
2023-06-01 02:13:25 +0800

    Registration validation

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-let text = document.getElementById("text")
+let Email = document.getElementById("email")
 let username1 = document.getElementById("username1")
 let password1 = document.getElementById("password1")
 
@@ -8,34 +8,28 @@ let login = document.getElementById("signup")
 let info = document.getElementById("info")
 
 
-function Signup() {
-    if (text.value == '') {
-        info.style = "color: red;"
-        info.innerHTML = "Information Required"
+function Signup() { 
+    if (Email.value == '' && username1.value == '' && password1.value == '') {
+        alert("Information Required")
     }
+    else if (Email.value == '') {
+        alert("Email Required")
+    }
+    
+    else if (username1.value == '') {
+        alert("Username Required")
+        
+    }
+
+    else if (password1.value == '') {
+        alert("Password Required")
+     }
+
     else {
-        localStorage.setItem("text", text.value)
+        localStorage.setItem("email", Email.value)
         localStorage.setItem("username1", username1.value)
         localStorage.setItem("password1", password1.value)
         alert("Registered Successfully")
         info.innerHTML = "Registered Successfully"
     }
 }
-
-window.addEventListener("keypress", (event) => {
-    if (event.key == "Enter") {
-        if (text.value == '') {
-            info.style = "color: red;"
-            info.innerHTML = "Information Required"
-        }
-        else {
-            localStorage.setItem("text", text.value)
-            localStorage.setItem("username1", username1.value)
-            localStorage.setItem("password1", password1.value)
-            info.style = "color: green;"
-            info.innerHTML = "Registered Successfully"
-            window.location.assign("main.html") 
-        }
-
-    }
-})
